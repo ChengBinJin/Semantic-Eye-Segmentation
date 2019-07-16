aa600828536e5d2de583704e8828aaf253993f47
chengbin <sbkim0407@gmail.com>
2019-07-16 09:50:37 +0900

    add json_read_id function

--- src/read_label.py
+++ src/read_label.py
@@ -1,6 +1,26 @@
 import os
+import json
 import cv2
 import numpy as np
+
+
+class JsonDataObj(object):
+    # stage - 0 semantic_segmentation_images
+    # stage - 1 generative_images
+    def __init__(self, file_path, stage=0):
+        file = open(file_path).read()
+        self.json_data = json.loads(file)
+        self.key = 'semantic_segmenation_images' if stage == 0 else "generative_images"
+
+    def find_id(self, target):
+        target = target.replace('.npy', '.png')
+
+        for i, item in enumerate(self.json_data):
+            for img_name in item[self.key]:
+                if img_name == target:
+                    user_id = item['id']
+                    return user_id
+
 
 def convert_color_label(img):
     yellow = [102, 255, 255]
@@ -19,32 +39,39 @@ def convert_color_label(img):
     return img_rgb
 
 
-def show_img(img, name):
-    # Move window to the specific fixed position
-    cv2.namedWindow(name)
-    cv2.moveWindow(name, 10, 10)
+def show_img(img, name, user_id=None):
+    full_name = name + ' - ' + user_id
 
-    cv2.imshow(name, img)
+    # Move window to the specific fixed position
+    cv2.namedWindow(full_name)
+    cv2.moveWindow(full_name, 10, 10)
+
+    cv2.imshow(full_name, img)
     if cv2.waitKey(0) & 0xff == 27:
         exit("Esc clicked")
 
-    cv2.destroyWindow(name)
+    cv2.destroyWindow(full_name)
 
 
-def read_labels(path):
-    filenames = [[os.path.join(path, fname), os.path.basename(fname)]
-                 for fname in os.listdir(path) if fname.endswith('.npy')]
+def read_labels(img_path, json_path):
+    filenames = [[os.path.join(img_path, fname), os.path.basename(fname)]
+                 for fname in os.listdir(img_path) if fname.endswith('.npy')]
+
+    jsonObj = JsonDataObj(json_path, stage=1)
 
     for filename, basename in filenames:
         label = np.load(filename)
 
         # Convert [0~3] labels to [0~255, 0~255, 0~255] BGR images
         label_rgb = convert_color_label(label)
-
+        # Find user id
+        user_id = jsonObj.find_id(target=basename)
         # Show image
-        show_img(img=label_rgb, name=basename)
+        show_img(img=label_rgb, name=basename, user_id=user_id)
 
 
 if __name__ == '__main__':
     test_label_path = '../../Data/OpenEDS/Generative_Dataset/test/labels/'
-    read_labels(test_label_path)
+    json_file_path = '../../Data/OpenEDS/OpenEDS_test_userID_mapping_to_images.json'
+    read_labels(test_label_path, json_file_path)
+
